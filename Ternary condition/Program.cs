4830c466f5c2e94784c6e4a09c1d8964c73a7a2c
amr mohamed elsayed <Amr0ooo0@outlook.com>
2018-07-23 16:26:41 +0200

    Add project files.

---
+++ Ternary condition/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ternary_condition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar = 10;
+                string myResult = myVar > 5 ? "great than 5" : "less than 5";
+            Console.WriteLine("My Result is {0}", myResult);
+
+
+
+            string myName = "Amr" , result ;
+            result = (myName.Equals("Amr")) ? "Yes" : "No";
+            Console.WriteLine("My Vaiable is {0}", result);
+                
+            Console.ReadKey();
+
+        }
+
+    }
+}
